32963b9ab9f76a227c10ef12f76e253400b6a6c2
muhluri_md <mudakamuhluri@gmail.com>
2025-09-29 12:31:33 +0200

    Removing mobile display features

--- main.js
+++ main.js
@@ -148,10 +148,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   });
 
-    const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-
-  });
